0466ecc2b929893f2520628f546f195e348e5697
vscabell <vscabell@student.42sp.org.br>
2021-05-05 00:21:35 +0200

    nginx pod with official image

--- setup.sh
+++ setup.sh
@@ -1,4 +1,40 @@
 #! /bin/bash
 
-minikube delete
-minikube start --driver=docker
+delete_environment()
+{
+    kubectl delete deployments --all 
+    kubectl delete services --all 
+    kubectl delete pods --all 
+    kubectl delete pvc --all 
+    kubectl delete pv --all 
+    minikube stop
+    minikube delete
+}
+
+clean_environment()
+{
+    kubectl delete deployments --all 
+    kubectl delete services --all 
+    kubectl delete pods --all 
+    kubectl delete pvc --all 
+    kubectl delete pv --all 
+}
+
+start_minikube()
+{
+    minikube start --driver=docker  
+}
+
+apply_config_files()
+{
+    kubectl apply -f srcs/k8s/
+}
+
+if [ "$1" == "apply" ]; then
+    clean_environment
+    apply_config_files
+else
+    delete_environment
+    start_minikube
+    apply_config_files
+fi
