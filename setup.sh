93346ef1849bceb0d3282b16cbcca100d4fba970
vscabell <vscabell@student.42sp.org.br>
2021-05-05 19:26:42 +0200

    add mysql - is not working

--- setup.sh
+++ setup.sh
@@ -49,8 +49,11 @@ build_images()
 
 apply_config_files()
 {
-    kubectl apply -f srcs/k8s/nginx.yaml
     kubectl apply -f srcs/k8s/metallb.yaml
+    kubectl apply -f srcs/k8s/nginx.yaml
+    kubectl apply -f srcs/k8s/mysql.yaml
+    kubectl apply -f srcs/k8s/phpmyadmin.yaml
+    kubectl apply -f srcs/k8s/wordpress.yaml
 }
 
 if [ "$1" == "apply" ]; then
