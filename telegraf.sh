fbfaa0821912f23fc6cdd1e937fb46257bada75e
vscabell <vscabell@student.42sp.org.br>
2021-05-18 23:33:33 +0200

    script to substitute telegraf files when necessary

---
+++ telegraf.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cp srcs/influxdb/srcs/telegraf.conf srcs/nginx/srcs/telegraf.conf 
+cp srcs/influxdb/srcs/telegraf.conf srcs/mysql/srcs/telegraf.conf 
+cp srcs/influxdb/srcs/telegraf.conf srcs/wordpress/srcs/telegraf.conf 
+cp srcs/influxdb/srcs/telegraf.conf srcs/phpmyadmin/srcs/telegraf.conf 
+cp srcs/influxdb/srcs/telegraf.conf srcs/grafana/srcs/telegraf.conf 
