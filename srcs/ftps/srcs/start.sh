bf27a53a8b75b392bcf8afb96c1ab57db28c4c8b
vscabell <vscabell@student.42sp.org.br>
2021-05-20 03:00:48 +0200

    ftps export data port and chenge some config

--- srcs/ftps/srcs/start.sh
+++ srcs/ftps/srcs/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 /usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
-# /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf &
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf &
+/usr/sbin/pure-ftpd -p 30000:30000 -P 192.168.49.2 &
 tail -F /dev/null
