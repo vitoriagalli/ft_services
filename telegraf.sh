68acf4e02e0ce5e79a1bfb42f870812fd58dad66
vscabell <vscabell@student.42sp.org.br>
2021-05-20 03:03:08 +0200

    ftps insert to script

--- telegraf.sh
+++ telegraf.sh
@@ -5,3 +5,4 @@ cp srcs/influxdb/srcs/telegraf.conf srcs/mysql/srcs/telegraf.conf
 cp srcs/influxdb/srcs/telegraf.conf srcs/wordpress/srcs/telegraf.conf 
 cp srcs/influxdb/srcs/telegraf.conf srcs/phpmyadmin/srcs/telegraf.conf 
 cp srcs/influxdb/srcs/telegraf.conf srcs/grafana/srcs/telegraf.conf 
+cp srcs/influxdb/srcs/telegraf.conf srcs/ftps/srcs/telegraf.conf 
