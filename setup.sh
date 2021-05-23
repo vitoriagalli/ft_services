a9bf7a6efba2fc9b9df19078a7bef89993f1e925
vscabell <vscabell@student.42sp.org.br>
2021-05-23 19:55:39 +0200

    improve setup script

--- setup.sh
+++ setup.sh
@@ -2,88 +2,173 @@
 
 start_environment()
 {
-    sudo chmod 666 /var/run/docker.sock 
-    minikube --cpus=2 --memory 3000 start --driver=docker
-    minikube addons enable storage-provisioner
-    minikube addons enable metrics-server
-    minikube addons enable dashboard
+    echo ""
+    echo -e "\033[1mStart minikube...\033[0m"
+
+    sudo chmod 666 /var/run/docker.sock && \
+    minikube --cpus=2 --memory 2200 start --driver=docker && \
+    minikube addons enable storage-provisioner && \
+    minikube addons enable metrics-server && \
+    minikube addons enable dashboard && \
     minikube addons enable ingress
+
+    if [ $? -ne 0 ]; then
+        echo "Error"
+        exit
+    fi
 }
 
 delete_environment()
 {
-    if [[ $(minikube status | grep -c "Running") != 0 ]]; then
-        minikube stop
-        minikube delete
-    fi
+    echo ""
+    echo -e "\033[1mDelete minikube...\033[0m"
+
+    minikube delete && \
     rm -rf ~/.kube/config
+
+    if [ $? -ne 0 ]; then
+        echo "Error"
+        exit
+    fi
 }
 
 apply_config()
 {
-    kubectl apply -f srcs/k8s/metallb.yaml
-    kubectl apply -f srcs/k8s/nginx.yaml
-    kubectl apply -f srcs/k8s/ftps.yaml
-    kubectl apply -f srcs/k8s/mysql.yaml
-    kubectl apply -f srcs/k8s/phpmyadmin.yaml
-    kubectl apply -f srcs/k8s/wordpress.yaml
-    kubectl apply -f srcs/k8s/grafana.yaml
-    kubectl apply -f srcs/k8s/influxdb.yaml 
+    echo ""
+    echo -e "\033[1mApply configuration files...\033[0m"
+
+    kubectl apply -f srcs/k8s/metallb.yaml && \
+    kubectl apply -f srcs/k8s/nginx.yaml && \
+    kubectl apply -f srcs/k8s/ftps.yaml && \
+    kubectl apply -f srcs/k8s/mysql.yaml && \
+    kubectl apply -f srcs/k8s/phpmyadmin.yaml && \
+    kubectl apply -f srcs/k8s/wordpress.yaml && \
+    kubectl apply -f srcs/k8s/grafana.yaml && \
+    kubectl apply -f srcs/k8s/influxdb.yaml
+
+    if [ $? -ne 0 ]; then
+        echo "Error"
+        exit
+    fi
 }
 
 clean_config()
 {
-    kubectl delete -f srcs/k8s/metallb.yaml
-    kubectl delete -f srcs/k8s/nginx.yaml
-    kubectl delete -f srcs/k8s/ftps.yaml
-    kubectl delete -f srcs/k8s/mysql.yaml
-    kubectl delete -f srcs/k8s/phpmyadmin.yaml
-    kubectl delete -f srcs/k8s/wordpress.yaml
-    kubectl delete -f srcs/k8s/grafana.yaml
-    kubectl delete -f srcs/k8s/influxdb.yaml 
+    echo ""
+    echo -e "\033[1mClean configuration files...\033[0m"
+
+    kubectl delete -f srcs/k8s/metallb.yaml && \
+    kubectl delete -f srcs/k8s/nginx.yaml && \
+    kubectl delete -f srcs/k8s/ftps.yaml && \
+    kubectl delete -f srcs/k8s/mysql.yaml && \
+    kubectl delete -f srcs/k8s/phpmyadmin.yaml && \
+    kubectl delete -f srcs/k8s/wordpress.yaml && \
+    kubectl delete -f srcs/k8s/grafana.yaml && \
+    kubectl delete -f srcs/k8s/influxdb.yaml
+
+    if [ $? -ne 0 ]; then
+        echo "Error"
+        exit
+    fi
 }
 
 build_images()
 {
-    docker build srcs/nginx -t nginx:vscabell
-    docker build srcs/ftps -t ftps:vscabell
-    docker build srcs/mysql -t mysql:vscabell
-    docker build srcs/phpmyadmin -t phpmyadmin:vscabell
-    docker build srcs/wordpress -t wordpress:vscabell
-    docker build srcs/grafana -t grafana:vscabell
-    docker build srcs/influxdb -t influxdb:vscabell  
+    echo ""
+    echo -e "\033[1mBuild images...\033[0m"
+
+    echo -n "Nginx image " && \
+    docker build srcs/nginx -t nginx:vscabell >> log_setup.txt && \
+    echo "created" && \
+
+    echo -n "FTPS image " && \
+    docker build srcs/ftps -t ftps:vscabell >> log_setup.txt && \
+    echo "created" && \
+
+    echo -n "MySQL image " && \
+    docker build srcs/mysql -t mysql:vscabell >> log_setup.txt && \
+    echo "created" && \
+
+    echo -n "PhpMyAdmin image " && \
+    docker build srcs/phpmyadmin -t phpmyadmin:vscabell >> log_setup.txt && \
+    echo "created" && \
+
+    echo -n "Wordpress image " && \
+    docker build srcs/wordpress -t wordpress:vscabell >> log_setup.txt && \
+    echo "created" && \
+
+    echo -n "Grafana image " && \
+    docker build srcs/grafana -t grafana:vscabell >> log_setup.txt && \
+    echo "created" && \
+
+    echo -n "InfluxDB image " && \
+    docker build srcs/influxdb -t influxdb:vscabell >> log_setup.txt
+    echo "created" && \
+
+    if [ $? -ne 0 ]; then
+        echo "Error"
+        exit
+    fi
 }
 
 clean_images()
 {
-    docker stop $(docker ps -aq --filter name=k8s)
-    docker rm $(docker ps -aq --filter name=k8s)
-    docker rmi -f nginx:vscabell
-    docker rmi -f ftps:vscabell
-    docker rmi -f mysql:vscabell
-    docker rmi -f phpmyadmin:vscabell
-    docker rmi -f wordpress:vscabell
-    docker rmi -f grafana:vscabell
-    docker rmi -f influxdb:vscabell  
+    echo ""
+    echo -e "\033[1mClean images...\033[0m"
+
+    docker stop $(docker ps -aq --filter name=k8s) && \
+    docker rm $(docker ps -aq --filter name=k8s) && \
+    docker rmi -f nginx:vscabell && \
+    docker rmi -f ftps:vscabell && \
+    docker rmi -f mysql:vscabell && \
+    docker rmi -f phpmyadmin:vscabell && \
+    docker rmi -f wordpress:vscabell && \
+    docker rmi -f grafana:vscabell && \
+    docker rmi -f influxdb:vscabell
+
+    if [ $? -ne 0 ]; then
+        echo "Error"
+        exit
+    fi
 }
 
 install_metallb()
 {
-    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-    kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+    echo ""
+    echo -e "\033[1mInstall metallb...\033[0m"
+
+    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml && \
+    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml && \
+    kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" && \
     minikube addons enable metallb
+
+    if [ $? -ne 0 ]; then
+        echo "Error"
+        exit
+    fi
 }
 
 atribute_ip()
 {
-    MINIKUBEIP=$(minikube ip)
-    IP=${MINIKUBEIP::-1}"$((${MINIKUBEIP: -1} + 1))"
-    sed -i "s/FIRSTIP-LASTIP/$IP-$IP/g" ./srcs/k8s/metallb.yaml
-    sed -i "s/IP/$IP/g" ./srcs/ftps/srcs/vsftpd.conf
+    echo ""
+    echo -e "\033[1mAtribute IP...\033[0m"
+
+    MINIKUBEIP=$(minikube ip) && \
+    IP=${MINIKUBEIP::-1}"$((${MINIKUBEIP: -1} + 1))" && \
+    sed -i "s/FIRSTIP-LASTIP/$IP-$IP/g" ./srcs/k8s/metallb.yaml && \
+    sed -i "s/IP/$IP/g" ./srcs/ftps/srcs/vsftpd.conf && \
     sed -i "s/CLUSTER_IP/$IP/g" ./srcs/mysql/srcs/wordpress.sql
+
+    if [ $? -ne 0 ]; then
+        echo "Error"
+        exit
+    fi
 }
 
+
+########## FT SERVICES ##########
+
+echo "" > log_setup.txt
 if [ "$1" == "apply" ]; then
     clean_config
     eval $(minikube docker-env)
@@ -102,5 +187,4 @@ else
     build_images
     apply_config
 fi
-
 minikube dashboard &> /dev/null &
