29cafa07083102a725405b5d95c1244e04fa034f
vscabell <vscabell@student.42sp.org.br>
2021-05-25 21:17:30 +0200

    add livenessProbe into mysql container

--- srcs/ftps/srcs/start.sh
+++ srcs/ftps/srcs/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 /usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf &
-tail -F /dev/null
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+# tail -F /dev/null
--- srcs/grafana/srcs/start.sh
+++ srcs/grafana/srcs/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 /usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
-grafana-server --homepath=/grafana-7.5.6 
-# tail -f /dev/null
+grafana-server --homepath=/grafana-7.5.6
