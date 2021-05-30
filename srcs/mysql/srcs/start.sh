5305f95dc6cac8fede7633dd2acdf9cd3bf04800
vscabell <vscabell@student.42sp.org.br>
2021-05-30 20:32:57 +0200

    change database creation config

--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -6,14 +6,14 @@ fi
 
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
 
-cat << EOF > config.sql
-CREATE DATABASE IF NOT EXISTS wordpress;
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' IDENTIFIED BY 'admin';
-FLUSH PRIVILEGES;
-EOF
+# cat << EOF > config.sql
+# CREATE DATABASE IF NOT EXISTS wordpress;
+# GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' IDENTIFIED BY 'admin';
+# FLUSH PRIVILEGES;
+# EOF
 
-chmod +w config.sql
+# chmod +w config.sql
 mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
-mysqld --user=root --skip_networking=0 --init-file=/config.sql & \
-sleep 10 && mysql wordpress < wordpress.sql;
+mysqld --user=root --skip_networking=0 --init-file=/wordpress.sql & \
+# sleep 10 && mysql wordpress < wordpress.sql;
 /usr/bin/telegraf --config /etc/telegraf/telegraf.conf
